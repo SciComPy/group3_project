9e967272cf1bc8e1fc6b5c45e03647b88296e153
csapatenio <camillepatenio@gmail.com>
2015-03-12 12:10:14 +0800

    second commit group project

--- group3_project.py
+++ group3_project.py
@@ -19,3 +19,13 @@ def stdDev(X):
     for x in X:
         tot += (x - mean)**2
     return (tot/len(X))**0.5
+
+def throwNeedles(numNeedles):
+    inCircle = 0
+    for Needles in xrange(1, numNeedles + 1, 1):
+        x = random.random()
+        y = random.random()
+        if (x*x + y*y)**0.5 <= 1.0:
+            inCircle += 1
+    return 4*(inCircle/float(numNeedles))
+ 
