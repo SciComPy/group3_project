850a4df23f1267aafb22b0cf7c96012d3aac51c3
krshsy <krshsy@gmail.com>
2015-03-12 12:01:21 +0800

    first commit group project

---
+++ group3_project.py
@@ -0,0 +1,21 @@
+import random, pylab
+ 
+#set line width
+pylab.rcParams['lines.linewidth'] = 6
+#set font size for titles
+pylab.rcParams['axes.titlesize'] = 20
+#set font size for labels on axes
+pylab.rcParams['axes.labelsize'] = 20
+#set size of numbers on x-axis
+pylab.rcParams['xtick.major.size'] = 5
+#set size of numbers on y-axis
+pylab.rcParams['ytick.major.size'] = 5
+#set font size for text
+pylab.rcParams['legend.fontsize'] = 20
+ 
+def stdDev(X):
+    mean = sum(X)/float(len(X))
+    tot = 0.0
+    for x in X:
+        tot += (x - mean)**2
+    return (tot/len(X))**0.5
